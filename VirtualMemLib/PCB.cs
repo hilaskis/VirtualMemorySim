50561225fb74fd328de12c9c90e0a6cba2da7918
Seth <hilaskis@mail.gvsu.edu>
2015-04-04 16:40:54 -0400

    Basic file input and process control block implementation

---
+++ VirtualMemLib/PCB.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMemLib
+{
+    /// <summary>
+    /// Class that represents a process control block for a proces
+    /// </summary>
+    public class PCB
+    {
+        private uint _NumPages = 0;     // Represents the number of pages for a process.
+        private uint _NumRef = 0;       // Represents the number of memory reference a process has made.
+        private string _ProcessName;
+
+        public PCB(string procName)
+        {
+            _ProcessName = procName;
+        }
+
+
+        public uint NumPages
+        {
+            get { return _NumPages; }
+            set { _NumPages = value; }
+        }
+        public uint NumRef
+        {
+            get { return _NumRef; }
+            set { _NumRef = value; }
+        }
+
+        public string ProcessName
+        {
+            get { return _ProcessName; }
+        }
+
+
+
+    }
+}
---
+++ VirtualMemSimConsole/StartMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace VirtualMemSimConsole
+{
+    class StartMain
+    {
+        
+        static void Main(string[] args)
+        {
+            String input_file = @"C:\Programs\VirtualMemSim\input3a.data";
+            StreamReader reader = new StreamReader(input_file);
+            String[] parsed_line;
+            String line;
+
+            while (!reader.EndOfStream)
+            {
+                line = reader.ReadLine();
+                parsed_line = line.Split(' ', '\t');
+                Console.WriteLine("Page Number Accessed: {0}", parsed_line[1]);
+            }
+        }
+    }
+}
